a2a182198b2aebde8ef07808a5d8d1fba2517ae6
Karl Bristav <karl.bristav@gmail.com>
2022-10-07 21:29:58 +0200

    Day 1

---
+++ AoC2019/Day01.fs
@@ -0,0 +1,28 @@
+﻿namespace AoC2019
+
+open Utils
+
+module Day01 =
+    let findFuelSimple (mass: int) : int = (mass / 3) - 2
+
+    let rec findFuelComplex (mass: int) : int =
+        let initalFuelReq = max 0 (findFuelSimple mass)
+
+        if initalFuelReq = 0 then
+            initalFuelReq
+        else
+            initalFuelReq + findFuelComplex initalFuelReq
+
+    let part1 (input: string) : string =
+        input
+        |> lines
+        |> Array.map (int >> findFuelSimple)
+        |> Array.sum
+        |> string
+
+    let part2 (input: string) : string =
+        input
+        |> lines
+        |> Array.map (int >> findFuelComplex)
+        |> Array.sum
+        |> string
---
+++ AoC2019/Utils.fs
@@ -0,0 +1,29 @@
+﻿namespace AoC2019
+
+open System
+
+module Utils =
+
+    let lines (input: string) : string [] =
+        input.Split([| "\r\n"; "\n"; "\r" |], StringSplitOptions.RemoveEmptyEntries)
+
+    let stringTrim (string: string) : string = string.Trim()
+
+    let words (input: string) : string [] =
+        input.Split([| " "; "\t" |], StringSplitOptions.RemoveEmptyEntries)
+        |> Array.map stringTrim
+
+    let commas (input: string) : string [] =
+        input.Split([| ", "; "," |], StringSplitOptions.RemoveEmptyEntries)
+
+    let isAllUppercase (input: string) : bool =
+        input |> Seq.forall (fun c -> Char.IsUpper c)
+
+    let twoArrayToTuple<'T> (arrayWithTwoElements: 'T []) : ('T * 'T) =
+        match arrayWithTwoElements with
+        | [| a; b |] -> (a, b)
+        | _ -> failwithf "Array does not contain exactly two elements! %A" arrayWithTwoElements
+
+    let eMod (a: int) (b: int) : int = ((a % b) + b) % b
+
+    let eMod64 (a: int64) (b: int64) : int64 = ((a % b) + b) % b
